677c33935e8ab8324d72a5f5e3de3776334bf4ed
Tushar R. Gosavi <tushar@apache.org>
2017-02-26 01:18:08 +0530

    Enable SSE

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,17 @@
 
 extern crate rlibc;
 
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() -> ! {
+  loop {}
+}
+
 #[no_mangle]
 pub extern fn rust_main() {
   let x = ["Hello", "World", "!"];
   let y = x;
+  let test = (0..3).flat_map(|x| 0..x).zip(0..);
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
