ae12f96930962e2f2f09f52b4def2d138f54151d
Tushar R. Gosavi <tushar@apache.org>
2017-02-26 23:52:45 +0530

    basic support for rust write macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 #![feature(lang_items)]
+#![feature(const_fn)]
+#![feature(unique)]
 #![no_std]
 
 extern crate rlibc;
+extern crate volatile;
+
+mod vga_buffer;
 
 #[allow(non_snake_case)]
 #[no_mangle]
@@ -11,17 +16,7 @@ pub extern "C" fn _Unwind_Resume() -> ! {
 
 #[no_mangle]
 pub extern fn rust_main() {
-  let hello = b"Hello World!";
-  let color_byte = 0x1f;
-
-  let mut hello_colored = [color_byte; 24];
-  for (i, char_byte) in hello.into_iter().enumerate() {
-    hello_colored[i*2] = *char_byte;
-  }
-
-  let buffer_ptr = (0xb8000 + 1988) as *mut _;
-  unsafe { *buffer_ptr = hello_colored };
-
+  vga_buffer::print_something();
   loop {}
 }
 
---
+++ src/vga_buffer.rs
@@ -0,0 +1,117 @@
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum Color {
+    Black      = 0,
+    Blue       = 1,
+    Green      = 2,
+    Cyan       = 3,
+    Red        = 4,
+    Magenta    = 5,
+    Brown      = 6,
+    LightGray  = 7,
+    DarkGray   = 8,
+    LightBlue  = 9,
+    LightGreen = 10,
+    LightCyan  = 11,
+    LightRed   = 12,
+    Pink       = 13,
+    Yellow     = 14,
+    White      = 15,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct ColorCode(u8);
+
+impl ColorCode {
+  const fn new(fg: Color, bg: Color) -> ColorCode {
+    ColorCode((bg as u8) << 4 | (fg as u8))
+  }
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+struct ScreenChar {
+  ascii_char : u8,
+  color : ColorCode,
+}
+
+
+const BUFFER_HEIGHT : usize = 25;
+const BUFFER_WIDTH  : usize = 80;
+
+use volatile::Volatile;
+
+struct Buffer {
+  chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT]
+}
+
+use core::ptr::Unique;
+
+pub struct Writer {
+   col_pos : usize,
+   color_code : ColorCode,
+   buffer: Unique<Buffer>,
+}
+
+impl Writer {
+
+  fn newline(&mut self) {
+     // TODO
+  }
+
+  fn buffer(&mut self) -> &mut Buffer {
+    unsafe { self.buffer.get_mut() }
+  }
+
+  pub fn write_byte(&mut self, byte : u8) {
+    match byte {
+       b'\n' => self.newline(),
+       byte => {
+         if (self.col_pos >= BUFFER_WIDTH) {
+           self.newline();
+         }
+
+         let row = BUFFER_HEIGHT - 1;
+         let col = self.col_pos;
+
+         let cc = self.color_code;
+         self.buffer().chars[row][col].write(ScreenChar {
+           ascii_char: byte,
+           color : cc
+         });
+         self.col_pos +=1;
+       }
+    }
+  }
+}
+
+use core::fmt;
+
+impl fmt::Write for Writer {
+  fn write_str(&mut self, s: &str) -> fmt::Result {
+    for byte in s.bytes() {
+      self.write_byte(byte);
+    }
+    Ok(())
+  }
+}
+
+pub fn print_something() {
+  use core::fmt::Write;
+  let mut writer = Writer {
+    col_pos : 0,
+    color_code : ColorCode::new(Color::LightGreen, Color::Black),
+    buffer: unsafe { Unique::new(0xb8000 as *mut _) },
+  };
+  writer.write_byte(b'H');
+  writer.write_str("ello!\n");
+  write!(writer, "The numbers are {} and {}", 42, 1.0/3.0);
+}
+
+pub static mut VGA : Writer = Writer {
+  col_pos : 0,
+  color_code : ColorCode::new(Color::White, Color::Black),
+  buffer: unsafe { Unique::new(0xb8000 as *mut _) },
+};
